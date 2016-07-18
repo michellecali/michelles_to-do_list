a4a14599461fb32d476351034d9b957380163ce4
Michelle Cali <mctexx@gmail.com>
2016-07-17 22:21:15 -0500

    rackup problems, about to dump useless code

--- app/controllers/list_controllers.rb
+++ app/controllers/list_controllers.rb
@@ -13,3 +13,18 @@ end
 MyApp.post "/all_tasks" do
 	redirect "/all_tasks"
 end
+
+MyApp.get "/new_task" do
+	@current_page = "create a new task"
+	erb :"/new_task"
+end
+
+MyApp.post "/new_task" do
+	@currentHash = List.getCurrentHash("todolist.txt")
+	@nextKey = List.getNextKey(@currentHash)
+	@newDescription = params['newDescription']
+	@newStatus = params['newStatus']
+	@newTask = "#{@nextKey}|#{@newDescription}#{@newStatus}"
+	List.writeNewTask("todolist.txt", @newTask)
+	redirect "/new_task"
+end
--- app/models/list_models.rb
+++ app/models/list_models.rb
@@ -12,6 +12,7 @@ class List
 		return currentHash
 	end
 
+
 	def List.getTaskStatus(currentHash)
 		taskStatus = {}
 		i = currentHash
@@ -32,32 +33,77 @@ class List
 		return taskDescription
 	end
 
-	# def List.printAllTasks(currentHash,taskStatus, taskDescription)
-	# 	printAllTasks = {}
-	# 	i = currentHash
-	# 	i.each do 
+	def List.getNextKey(currentHash)
+		nextKey = currentHash.length
+		return nextKey
+	end
 
-
-
-
-	# def List.addNewTask(filename, newString)
-	# 	i = newString
-	# 	newFile = File.open(filename, "w")
-	# 	newFile.print i
-	# 	newFile.close
+	# def List.updateTaskDescription(taskDescription, nextKey, newDescription)
+	# 	i = nextKey
+	# 	j = newDescription
+	# 	taskDescription[i] = j
+	# 	return taskDescription
 	# end
+
+	# def List.updateTaskStatus (taskStatus, nextKey, newStatus)
+	# 	i = nextKey
+	# 	j = newStatus
+	# 	taskStatus[i] = j
+	# 	return taskStatus
+	# end
+
+	# def List.updateHash(nextKey, newHashValue, currentHash)
+	# 	newHash = currentHash
+	# 	newHash[nextKey] = newHashValue
+	# 	return newHash
+	# end
+
+	# def List.makeNewArray(newHash)
+	# 	newArray = Array.new
+	# 	i = newHash
+	# 	i.each do |j, k|
+	# 		l = j
+	# 		m = k
+	# 		n = l + "|" + m
+	# 		newArray << n
+	# 	end
+	# 	return newArray
+	# end
+
+	def List.writeNewTask(filename, newTask)
+		i = newTask
+		newFile = File.open(filename, "a")
+		newFile.puts i
+		newFile.close
+	end
+
 end
 
 # # for local testing
-# x = List.getCurrentHash("todolist.txt")
-# print x
-
+x = List.getCurrentHash("todolist.txt")
+print x
 
 # y = List.getTaskStatus(x)
 # print y
 
 # z = List.getTaskDescription(x)
 # print z
+
+# w = List.getNextKey(x)
+# print w 
+
+# v = List.updateTaskDescription(z, w, "Go to the store")
+# # print v
+
+# u = List.updateTaskStatus(y, w, "i")
+# # print u
+
+# a = List.updateHash(w, "Go to the storei", x)
+# puts "appended"
+# print a
+
+# b = List.makeNewArray(a)
+# puts b
 
 
 
